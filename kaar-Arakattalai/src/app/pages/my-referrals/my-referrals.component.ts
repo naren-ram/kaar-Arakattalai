0b9ca493d018603c76dd82ea7df8a909f304a005
Anbarasi1-t <tanbarasi@kaartech.com>
2025-07-29 14:44:52 +0530

    Errors Cleared

--- kaar-Arakattalai/src/app/pages/my-referrals/my-referrals.component.ts
+++ kaar-Arakattalai/src/app/pages/my-referrals/my-referrals.component.ts
@@ -1,20 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-<<<<<<< HEAD
-import { CommonModule } from '@angular/common';
+import { ReferralTableComponent } from '../../components/referral-table/referral-table.component';
+import { MenubarComponent } from '../../components/menubar/menubar.component';
 
 @Component({
   selector: 'app-my-referrals',
   standalone: true,
-  imports: [CommonModule],
-=======
-import { ReferralTableComponent } from '../../components/referral-table/referral-table.component';
-import  { MenubarComponent } from '../../components/menubar/menubar.component';
-@Component({
-  selector: 'app-my-referrals',
-  standalone: true,
-  imports: [MenubarComponent,ReferralTableComponent],
->>>>>>> dcd1f9ae03a6747e44ff99a02ab49038715c0aea
+  imports: [MenubarComponent, ReferralTableComponent],
   templateUrl: './my-referrals.component.html',
   styleUrls: ['./my-referrals.component.scss']
 })
