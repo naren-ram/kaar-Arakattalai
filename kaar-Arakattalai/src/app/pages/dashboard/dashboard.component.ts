3da145210f0ed228bf4ae8ac4881af24a8d55abb
Aiswarya G <aiswarya3325@gmail.com>
2025-08-06 14:19:02 +0530

    n

--- kaar-Arakattalai/src/app/pages/dashboard/dashboard.component.ts
+++ kaar-Arakattalai/src/app/pages/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { MenubarComponent } from '../../components/menubar/menubar.component';
 import { ReferralTableComponent } from '../../components/referral-table/referral-table.component';
 import { SummaryConsComponent } from '../../components/summary-cons/summary-cons.component';
-import { HeaderComponent } from '../../header/header.component';
+
 import { UserProfileComponent } from '../../components/user-profile/user-profile.component';  
 @Component({
   selector: 'dashboard',
@@ -13,7 +13,7 @@ import { UserProfileComponent } from '../../components/user-profile/user-profile
     UserProfileComponent,
     SummaryConsComponent,
     ReferralTableComponent,
-    HeaderComponent,
+  
     
   ],
   templateUrl: './dashboard.component.html',
